66a94c6491e640e95cc4fb8c0dffa3c3f276a37c
DianVK <diankinanev@abv.bg>
2023-03-06 09:05:59 +0200

    Add the ages

---
+++ 10 SQL Code Challenges/10. Add the ages.sql
@@ -0,0 +1,16 @@
+CREATE TABLE People (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Name VARCHAR(6),
+    Age INT
+);
+
+INSERT INTO People(Name, Age) VALUES 
+    ('Bob', 21),
+    ('Sam', 19),
+    ('Jill', 18),
+    ('Jim', 21),
+    ('Sally', 19),
+    ('Jess', 20),
+    ('Will', 21);
+
+SELECT SUM(Age) FROM People;
